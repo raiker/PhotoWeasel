4194130c2796bdd199643f9fe6fc7a8d0a97fc6f
KJTsanaktsidis <kjtsanaktsidis@gmail.com>
2012-06-11 22:56:34 +1000

    Fixed nHibernate and reshuffled the project
    Signed-off-by: KJTsanaktsidis <kjtsanaktsidis@gmail.com>

---
+++ PWDBTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using PhotoWeasel.domain;
+using System.Data.SQLite;
+
+namespace PWDBTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(typeof(Photo).Assembly);
+
+            new SchemaExport(cfg).Execute(false, true, false);
+            Console.ReadLine();
+        }
+    }
+}
